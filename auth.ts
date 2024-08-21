72e22bc9cffed87c24dc4152ec4efc20d2ee1d77
Craig Haseler <cahaseler@users.noreply.github.com>
2024-08-21 13:47:42 -0400

    feat: configuration of Login.gov as a custom NextAuth provider

--- auth.ts
+++ auth.ts
@@ -1,5 +1,23 @@
 import NextAuth from "next-auth";
 
-export const { handlers, signIn, signOut, auth } = NextAuth({
-  providers: [],
+export const { handlers, auth } = NextAuth({
+  debug: true,
+  providers: [
+    {
+      id: "logindotgov",
+      name: "Login.gov",
+      type: "oidc",
+      issuer: "https://idp.int.identitysandbox.gov/", // to infer the .well-known/openid-configuration URL
+      clientId: process.env.AUTH_CLIENT_ID, // from Login.gov's app dashboard
+      authorization: {
+        params: { acr_values: "http://idmanagement.gov/ns/assurance/loa/1" },
+        // Change this to specify different requirements for authentication, such as requiring identify verification, PIV, etc. Choose from options listed here:
+        // https://idp.int.identitysandbox.gov/.well-known/openid-configuration
+      },
+      checks: ["pkce", "state", "nonce"],
+      client: {
+        token_endpoint_auth_method: "none", // This is the key to making Login.gov work since it doesn't issue a client secret.
+      },
+    },
+  ],
 });
